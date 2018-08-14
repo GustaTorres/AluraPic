67dfcc03b899bedc1d82143b6abed7973f371fb0
Gustavo <gustavo.s.torres@hotmail.com>
2018-08-13 21:16:07 -0300

    aula 02 concluida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'alurapic';
+
+  photos = [
+    {
+      url: "https://mcn-images.bauersecure.com/PageFiles/488045/Triumph675StreetTriple.jpg",
+      description: "street triple"
+    },
+    {
+      url: "https://www.thehindu.com/life-and-style/motoring/article18351587.ece/alternates/FREE_660/03bgmtriumph4jpg",
+      description: "Tiger 800"
+    }
+  ]
 }
