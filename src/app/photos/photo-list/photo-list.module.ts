30c0d97cfd000f8d11afd226de78bd632b6edb55
Gustavo <gustavo.s.torres@hotmail.com>
2018-08-23 23:48:09 -0300

    Aula 07 não finalizada

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,22 @@
+import { CardModule } from './../../shared/components/card/card.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhotoModule } from './../photo/photo.module';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotosComponent } from './photos/photos.component';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { FilterByDescriptionPipe } from './filter-by-description.pipe';
+
+@NgModule({
+    declarations: [
+       PhotoListComponent,
+        PhotosComponent,
+        LoadButtonComponent,
+        FilterByDescriptionPipe
+    ],imports:[
+        CommonModule,
+        PhotoModule,
+        CardModule
+    ]
+})
+export class PhotoListModule {}
